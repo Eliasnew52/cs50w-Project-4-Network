c80fa7f961641df26f161847dc962fe66b62c19f
Eliasnew52 <xxeliasnew52xx@gmail.com>
2024-02-02 10:54:16 -0600

    Post Model Fixed (now it runs)

--- network/models.py
+++ network/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    Content = models.CharField(max_length=400)
+    Author = models.ForeignKey(User,on_delete=models.CASCADE, related_name="Author")
+    Date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Post {self.id} made by {self.Author} on {self.Date.strftime('%d %b %Y %H:%M:%M:%S')}"
